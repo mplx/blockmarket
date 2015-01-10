dea63a3e8ac176043d1b3cb53b1a70c857d806ae
mplx <mplx+coding@donotreply.at>
2015-01-10 02:22:01 +0100

    added comments to main controller

--- src/mplx/blockmarket/Module/Main/Controller/IndexController.class.php
+++ src/mplx/blockmarket/Module/Main/Controller/IndexController.class.php
@@ -29,14 +29,17 @@ class IndexController extends AbstractMainController
 
     protected function executeStock()
     {
+        // initialize
+        $data = array();
+
+        // which one?
         if (!isset($_GET['stockid']) || !is_numeric($_GET['stockid'])) {
             $id = 87;
         } else {
             $id = $_GET['stockid'];
         }
 
-        $data = array();
-
+        // get stock
         $temp = $this->db->getStocks($id);
         if (! $temp) {
             return $this->twig->render('error.tpl.html');
@@ -44,6 +47,7 @@ class IndexController extends AbstractMainController
             $data['basic'] = $temp[0];
         }
 
+        // meta data
         if (isset($data['basic']['title_wiki'])) {
             $data['basic']['url']['wiki'] = BM_WIKI_URL . 'wiki/' . $data['basic']['title_wiki'];
             //$data['basic']['url']['icon'] = BM_WIKI_URL . 'wiki/File:' . $data['basic']['title_wiki'] . '_Icon.png';
@@ -52,11 +56,13 @@ class IndexController extends AbstractMainController
             $data['basic']['url']['icon'] = BM_WIKI_URL . $data['basic']['icon_path'];
         }
 
+        // config
         $data['config']['timezone']['zone'] = date("e");
         $data['config']['timezone']['gmtdiff'] = date("O");
         $data['config']['timezone']['gmtdiffhours'] = substr($data['config']['timezone']['gmtdiff'], 0, 3);
         $data['config']['wiki_url'] = BM_WIKI_URL;
 
+        // favorites
         if (bm_COOKIE('favorites')) {
             $query = '';
             $result = preg_match_all('/([0-9]+)/', bm_COOKIE('favorites'), $lookup);
@@ -78,6 +84,7 @@ class IndexController extends AbstractMainController
             }
         }
 
+        // last 100
         $query = sprintf(
             "SELECT marketvalue, UNIX_TIMESTAMP(ts)*1000 AS tstamp " .
             "FROM prices " .
@@ -88,6 +95,7 @@ class IndexController extends AbstractMainController
         $temp = $this->db->query($query);
         $data['hundret'] = array_reverse($temp);
 
+        // 24h
         $query = sprintf(
             "SELECT marketvalue, UNIX_TIMESTAMP(ts)*1000 AS tstamp " .
             "FROM prices " .
@@ -96,6 +104,8 @@ class IndexController extends AbstractMainController
         );
         $temp = $this->db->query($query);
         $data['twentyfour'] = $temp;
+
+        // market data
         $data['current']['marketvalue'] = $data['twentyfour'][count($data['twentyfour'])-1]['marketvalue'];
         $units = 1;
         if ($data['current']['marketvalue']<=0.009999) {
@@ -109,6 +119,7 @@ class IndexController extends AbstractMainController
         $data['current']['coppercoins'] =
             round(($data['current']['marketvalue'] * $units - $data['current']['platinumcoins'] * 100 - $data['current']['goldcoins']) * 100);
 
+        // last month averages
         $query = "SELECT MAX(marketvalue) AS pricemax, MIN(marketvalue) AS pricemin, AVG(marketvalue) AS priceavg, date(ts) AS bmdate, UNIX_TIMESTAMP(date(ts))*1000 AS tstamp " .
                 "FROM prices " .
                 "WHERE stock_id = %d AND date(ts)>=DATE_SUB(NOW(), INTERVAL 1 MONTH) " .
@@ -118,6 +129,7 @@ class IndexController extends AbstractMainController
         $temp = $this->db->query($query);
         $data['month'] = $temp;
 
+        // render template
         return $this->twig->render(
             'module/main/index/index.tpl.html',
             array(
